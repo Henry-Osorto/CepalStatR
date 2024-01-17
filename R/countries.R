96e45ee21f8338822110db575d93026056832a71
Henry-Osorto <henryantonioosortoruiz@gmail.com>
2024-01-17 11:02:53 -0600

    Se agregó la función countries

---
+++ R/countries.R
@@ -0,0 +1,37 @@
+#' list of countries
+#'@description
+#'A short description...
+#'
+#' @param language.en
+#' If TRUE or omitted is selected, the default language will be English.
+#' Select FALSE to choose the Spanish language.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' data <- countries()
+#'
+#'
+countries <- function(language.en = TRUE) {
+
+  if(language.en == TRUE){
+
+    data <- call.data(id.indicator = 1, language.en = TRUE)
+
+    country <- data.frame(Countries = unique(data$Country))
+  }
+
+  else {
+    data <- call.data(id.indicator = 1, language.en = FALSE)
+
+    country <- data.frame(Paises = unique(data$País))
+
+  }
+
+
+  return(country)
+}
+
+
+pais <- country.list(language.en = F)
