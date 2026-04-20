c92d8f125f64e8e3f387ec2d7b09f8d9c6ce5087
Henry-Osorto <henryantonioosortoruiz@gmail.com>
2026-04-19 23:11:54 -0600

    Creación de globalVariables

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+utils::globalVariables(c(
+  ".data",
+  "Edad", "Edad_num", "Maximo", "Sex_plot",
+  "Valor", "Value", "filtro", "valor", "value",
+  "indicator_id", "level_2", "name", "node_name"
+))
