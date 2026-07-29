7762d419eb2630415caf97911cb6b318b5e7cd10
Henry-Osorto <henryantonioosortoruiz@gmail.com>
2026-07-28 18:45:06 -0600

    Update `%||%` <- function(x, y) {   if (is.null(x)) y else x }

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -1,16 +1,12 @@
-#' Pipe operator
-#'
-#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
-#'
-#' @name %>%
-#' @rdname pipe
-#' @keywords internal
-#' @export
-#' @importFrom magrittr %>%
-#' @usage lhs \%>\% rhs
-#' @param lhs A value or the magrittr placeholder.
-#' @param rhs A function call using the magrittr semantics.
-#' @return The result of calling `rhs(lhs)`.
-NULL
-
-`%||%` <- function(x, y) if (is.null(x)) y else x
+# Internal null-coalescing operator
+#
+# Returns `y` when `x` is `NULL`; otherwise returns `x`.
+#
+# @param x Primary value.
+# @param y Replacement value used when `x` is `NULL`.
+#
+# @return Either `x` or `y`.
+# @keywords internal
+`%||%` <- function(x, y) {
+  if (is.null(x)) y else x
+}
