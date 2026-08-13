90cdc930d7d386ce3a6e195f1f63af64f6bff019
Henry-Osorto <henryantonioosortoruiz@gmail.com>
2026-08-13 11:43:43 -0600

    Update utils-dimensions

--- R/utils-dimensions.R
+++ R/utils-dimensions.R
@@ -3,15 +3,19 @@ get_cepal_dimensions <- function(
     id.indicator,
     lang = "en"
 ) {
-  url <- paste0(
-    "https://api-cepalstat.cepal.org",
-    "/cepalstat/api/v1/indicator/",
-    id.indicator,
-    "/dimensions",
-    "?lang=", lang,
-    "&format=json",
-    "&in=1",
-    "&path=0"
+
+  url <- cepalstat_build_url(
+    path = paste0(
+      "indicator/",
+      id.indicator,
+      "/dimensions"
+    ),
+    query = list(
+      lang = lang,
+      format = "json",
+      `in` = 1,
+      path = 0
+    )
   )
 
   cepal_get(
